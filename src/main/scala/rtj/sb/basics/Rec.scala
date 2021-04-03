253330b24077f6cc25381986e89b9a517c279e8d
Erlend Aakre <erlend@aakre.io>
2021-04-03 12:38:36 +0100

    rec

--- src/main/scala/rtj/sb/basics/Rec.scala
+++ src/main/scala/rtj/sb/basics/Rec.scala
@@ -29,4 +29,42 @@ object Rec extends App {
   println(factorial2(5000))
 
 
+  def concat(s: String, n:Int): String = {
+    @tailrec
+    def go(i: Int, acc: String): String = {
+      if(i <= 1) acc
+      else go(i-1, acc+s)
+    }
+    go(n,s)
+  }
+
+  println(concat("lol",15))
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def go(nn: Int, stillPrime: Boolean): Boolean = {
+      if(!stillPrime) false
+      else if(nn <= 1) true
+      else go(nn-1, n % nn != 0 && stillPrime)
+    }
+    go(n/2, true)
+  }
+
+  println(isPrime(2003))
+  println(isPrime(125))
+
+
+
+  def fib(n: Int): Long = {
+    @tailrec
+    def go(i: Int, previous: Long, current: Long): Long = {
+      if (i >= n) current
+      else go(i+1,current, current+previous)
+    }
+
+    go(3, 1,2)
+  }
+
+  println(fib(8))
+
 }
