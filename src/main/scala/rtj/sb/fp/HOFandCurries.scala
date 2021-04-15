01e34d2589c5af3fc2cc4846bdca75176926e803
Erlend Aakre <erlend@aakre.io>
2021-04-15 09:07:21 +0100

    formatting

--- src/main/scala/rtj/sb/fp/HOFandCurries.scala
+++ src/main/scala/rtj/sb/fp/HOFandCurries.scala
@@ -5,23 +5,23 @@ import scala.annotation.tailrec
 object HOFandCurries extends App {
 
   // HOF - Higher Order Functions
-//   val superFunc: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = ???
+  //   val superFunc: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = ???
 
   // function that applies function n times to a value x
   // nTimes(f,n,x)
   @tailrec
   def nTimes(f: Int => Int, n: Int, x: Int): Int = {
-    if(n <= 0) x
-    else nTimes(f, n-1, f(x))
+    if (n <= 0) x
+    else nTimes(f, n - 1, f(x))
   }
 
-  val plusOne = (n: Int) => n+1
+  val plusOne = (n: Int) => n + 1
   println(nTimes(plusOne, 5, 10))
 
   // Curried
   def nTimesCurried(f: Int => Int, n: Int): (Int => Int) =
-    if(n <= 0) (x:Int) => x
-    else (x:Int) => nTimesCurried(f, n-1)(f(x))
+    if (n <= 0) (x: Int) => x
+    else (x: Int) => nTimesCurried(f, n - 1)(f(x))
 
   val plusTen = nTimesCurried(plusOne, 10)
   println(plusTen(20))
