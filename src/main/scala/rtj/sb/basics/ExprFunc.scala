7a28b18ba363131b6233eb912330430f4f23c9c9
Erlend Aakre <erlend@aakre.io>
2021-08-16 20:45:58 +0100

    repeat() + isPrime()

--- src/main/scala/rtj/sb/basics/ExprFunc.scala
+++ src/main/scala/rtj/sb/basics/ExprFunc.scala
@@ -25,11 +25,17 @@ object ExprFunc extends App {
   }
   print()
 
+  def repeat(s: String, n: Int): String = {
+    if(n <= 1) s
+    else s + repeat(s, n-1)
+  }
+  println(repeat("shit! ", 10))
+
   def fac(n: Int): Long = {
     @tailrec
-    def go(acc: Long, nn: Int): Long =
-      if(nn == n) acc*n
-      else go(acc*nn , nn+1)
+    def go(acc: Long, i: Int): Long =
+      if(i == n) acc*n
+      else go(acc*i , i+1)
     go(1, 1)
   }
   println(fac(5))
@@ -39,4 +45,26 @@ object ExprFunc extends App {
     else fib(n-1) + fib(n-2)
   }
   println(fib(7))
+
+  def isPrime(n: Long) = {
+    assert(n >= 1, "isPrime only take positive integers")
+    println(s"is $n prime?")
+    @tailrec
+    def go(i: Int): Boolean = {
+      if(i == n/2) true
+      else {
+        if(n % i == 0) false
+        else go(i+1)
+      }
+    }
+    go(2)
+  }
+
+  println(isPrime(5))
+  println(isPrime(6))
+  println(isPrime(21))
+  println(isPrime(47))
+  println(isPrime(48))
+  println(isPrime(606))
+  println(isPrime(607))
 }
